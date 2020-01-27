7c748fa35f7a5a30590cd3bf95aceb02e230e479
Alexey <vorresti@ya.ru>
2020-01-27 13:49:19 +0300

    Modals added

--- js/ui/Modal.js
+++ js/ui/Modal.js
@@ -13,7 +13,12 @@ class Modal {
    * необходимо выкинуть ошибку.
    * */
   constructor( element ) {
-
+    if(element) {
+      this.element = element;
+      this.registerEvents();
+    } else {
+      throw new Error(`Error: element not exist`);
+    }
   }
 
   /**
@@ -22,7 +27,10 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
+    const closeButtons = this.element.querySelectorAll('[data-dismiss="modal"]')
+    closeButtons.forEach(element => {
+      element.addEventListener('click', e => this.onClose(e));
+    });
   }
 
   /**
@@ -30,25 +38,29 @@ class Modal {
    * Закрывает текущее окно (Modal.close())
    * */
   onClose( e ) {
-
+    e.preventDefault();
+    this.close();
   }
   /**
    * Удаляет обработчики событий
    * */
   unregisterEvents() {
-
+    const closeButtons = this.element.querySelectorAll('[data-dismiss="modal"]')
+    closeButtons.forEach(element => {
+      element.removeEventListener('click', this.onClose)
+    });
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+    this.element.style.setProperty('display', 'block');
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
-  close(){
-
+  close() {
+    this.element.style.removeProperty('display');
   }
 }
