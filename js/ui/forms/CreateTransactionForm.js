3755859578f4eec310ccc71facc959afd81b3384
Alexey <vorresti@ya.ru>
2020-01-28 13:52:35 +0300

    CreateTransactionForm added

--- js/ui/forms/CreateTransactionForm.js
+++ js/ui/forms/CreateTransactionForm.js
@@ -3,13 +3,15 @@
  * создания новой транзакции
  * Наследуется от AsyncForm
  * */
-class CreateTransactionForm {
+class CreateTransactionForm extends AsyncForm {
   /**
    * Вызывает родительский конструктор и
    * метод renderAccountsList
    * */
   constructor( element ) {
-
+    super(element);
+    this.element = element;
+    this.renderAccountsList();
   }
 
   /**
@@ -17,7 +19,27 @@ class CreateTransactionForm {
    * Обновляет в форме всплывающего окна выпадающий список
    * */
   renderAccountsList() {
+    if (User.current()) {
+      const dropDownList = document.querySelectorAll("select.accounts-select");
 
+      Account.list(User.current(), (err, data) => {
+  
+        dropDownList.forEach(elem => elem.innerHTML = '');
+  
+        if (data.success) {
+          let actualAccounts = data.data;
+          for (let i = 0; i < actualAccounts.length; i++) {
+            let accountsItem = actualAccounts[i];
+            let option = `<option value="${accountsItem.id}">${accountsItem.name}</option>`;
+            for (let listItem of dropDownList) {
+              listItem.insertAdjacentHTML('beforeend', option);
+            }
+          }
+        } else {
+          console.log(`Error: ${err}`);
+        }
+      });
+    }
   }
 
   /**
@@ -27,6 +49,21 @@ class CreateTransactionForm {
    * в котором находится форма
    * */
   onSubmit( options ) {
+    Transaction.create(options, (err, data) => {
 
+      if (data.success) {
+        this.element.reset();
+
+        // let modal = new Modal(this.element.closest('.modal'));
+        // modal.close();
+        new Modal(this.element.closest('.modal')).close();
+
+        App.update();
+      } else {
+        alert(data.error);
+        console.log(`Error: ${err}`);
+        //return;
+      }
+    });
   }
 }
